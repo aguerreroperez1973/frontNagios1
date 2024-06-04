42e135acd49f12ff668b6147b3c731b4ba4fe7c1
aguerreroperez1973 <aguerreroperez@hotmail.com>
2024-06-04 13:47:05 -0400

    Se crea refresh automatico

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -4,7 +4,7 @@ export const Context = createContext({});
 
 const ContextProvider = ({ children }) => {
 
-    const url= "./public/status.json";
+    const url= "./status.json";
     const [dataHosts, setDataHosts] = useState([]);
     const [dataServices, setDataServices] = useState([]);
     
@@ -21,6 +21,8 @@ const ContextProvider = ({ children }) => {
         };
             
       useEffect(() => { consultarApi(); }, []);
+
+      setTimeout(()=>{ consultarApi(); }, 20000);
       
           return (
                   < Context.Provider value={{dataHosts, dataServices}} > {children} </Context.Provider>
